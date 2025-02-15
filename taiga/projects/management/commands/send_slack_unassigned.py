9f950e60d99258219e0de67f003f954cf033bd32
Alex de Landgraaf <alex@maykinmedia.nl>
2025-02-15 12:27:48 +0100

    Sending unassigned issues on an hourly basis to slack

--- taiga/projects/management/commands/send_slack_unassigned.py
+++ taiga/projects/management/commands/send_slack_unassigned.py
@@ -12,39 +12,16 @@ from django.utils import timezone
 
 from taiga.projects.issues.models import Issue
 
-SEVERITY_CHOICES = ["wishlist", "minor", "normal", "important", "critical"]
-PRIORITY_CHOICES = ["low", "normal", "high"]
-
 
 class Command(BaseCommand):
-    help = "Sends Slack notifications if a specific priority/severity is set on a new issue."
+    help = "Sends Slack notifications for recently-added issues that weren't assigned"
 
     def add_arguments(self, parser: CommandParser) -> None:
         parser.add_argument(
             "--last-updated",
             type=int,
-            default=5,
-            help="Send notifications for the issues created in the last x minutes. Default: 5."
-        )
-        parser.add_argument(
-            "--severity",
-            choices=SEVERITY_CHOICES,
-            default="critical",
-            help=(
-                "Select issues matching the provided severity, or higher. "
-                "If 'important' is selected, all issues with severity 'important' or 'critical' "
-                "will be selected (using icontains on the name). Default: 'critical'."
-            )
-        )
-        parser.add_argument(
-            "--priority",
-            choices=PRIORITY_CHOICES,
-            default="high",
-            help=(
-                "Select issues matching the provided priority, or higher. "
-                "If 'normal' is selected, all issues with severity 'normal' or 'high' "
-                "will be selected (using icontains on the name). Default: 'high'."
-            )
+            default=60,
+            help="Send notifications for the issues created in the last x minutes. Default: 60."
         )
         parser.add_argument(
             "--blacklist",
@@ -54,28 +31,17 @@ class Command(BaseCommand):
         )
 
     def send_message(self, message: str, channel: str):
-        self.client.chat_postMessage(channel=settings.SLACK_CHANNEL,
+        self.client.chat_postMessage(settings.SLACK_CHANNEL,
                                      link_names=1,
                                      text=message)
 
-    def get_issues(self, severity: str, priority: str, last_updated: int, blacklist: Optional[List[Union[str, int]]]) -> QuerySet:
+    def get_issues(self, last_updated: int, blacklist: Optional[List[Union[str, int]]]) -> QuerySet:
         now = timezone.now()
         time_diff = now - datetime.timedelta(seconds=last_updated * 60)
 
-        # OR filter with all severity/priority possibilities
-        severity_filters = reduce(
-            lambda q1, q2: q1 | q2,
-            [Q(severity__name__icontains=severity) for severity in SEVERITY_CHOICES[SEVERITY_CHOICES.index(severity):]]
-        )
-
-        priority_filters = reduce(
-            lambda q1, q2: q1 | q2,
-            [Q(priority__name__icontains=priority) for priority in PRIORITY_CHOICES[PRIORITY_CHOICES.index(priority):]]
-        )
-
         issues = Issue.objects.filter(
-            severity_filters | priority_filters,
             created_date__gte=time_diff,
+            assigned_to__isnull=True,
             status__is_closed=False
         )
 
@@ -87,7 +53,7 @@ class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any) -> str:
         self.client = WebClient(token=settings.SLACK_BOT_TOKEN)
 
-        issues = self.get_issues(options["severity"], options["priority"], options["last_updated"], options["blacklist"])
+        issues = self.get_issues(options["last_updated"], options["blacklist"])
         self.issues = issues
 
         if issues.exists():
@@ -96,8 +62,7 @@ class Command(BaseCommand):
 
             # Should use Django pluralization instead, but messages should be sent in English
             message = (
-                f"Taiga alert @channel: the following issue{'s were' if issues.count() > 1 else ' was'} recently set to {options['severity']}/{options['priority']}"
-                f"{' or higher' if options['severity'] != 'critical' or options['priority'] != 'high' else ''}:\n"
+                f"Taiga info: the following issue{'s were' if issues.count() > 1 else ' was'} recently added but not assigned:\n"
             )
             message += issues_str
             self.stdout.write(f"The following issues were found:\n{issues_str}")
